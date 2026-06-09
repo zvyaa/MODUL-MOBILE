5affbe86b84f9bf3f8727383adaacbb568491b61
Amandha <amandhacitra05@gmail.com>
2026-06-09 22:30:29 +0800

    Delete MODUL5 directory

--- MODUL5/MyMovieAppM5/app/src/main/java/com/example/mymovieappm5/util/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mymovieappm5.util
-
-object Constants {
-    const val API_KEY = "b61a592770cb876e92e77cdc349dfc59"
-    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
-    const val CACHE_DURATION = 60 * 60 * 1000L
-    const val CATEGORY_POPULAR = "popular"
-    const val CATEGORY_NOW_PLAYING = "now_playing"
-}
